9e0e2767fa54839595c7bdf23b202f6cc40e706f
Meyers-John <mrmeyersmakerspace@gmail.com>
2020-06-26 12:13:46 -0600

    Initial Creation

---
+++ src/contents/Instructions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Instructions extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is how you use the application</h1>
+                <article>These are the instructions on how to use the application.</article>
+            </div>
+        )
+    }
+}
